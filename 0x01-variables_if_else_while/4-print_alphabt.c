d7b9235e72dd4ccd7d0f29876f0821d17024b933
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-17 22:10:13 +0000

    k

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int ch;
-	for (ch = 'a' ;ch <=  'z'; ch++)
+
+	for (ch = 'a' ; ch <=  'z'; ch++)
 	{
-		if (ch != 'e' && ch !='q')
+		if (ch != 'e' && ch != 'q')
 			putchar(ch);
 	}
 	putchar('\n');
