b4865c80932e5564aa7084b9968e0f530d01cf64
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-17 23:06:39 +0000

    k

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print combination of numbers
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+	{
+		num = num % 10;
+		putchar(num +'0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
