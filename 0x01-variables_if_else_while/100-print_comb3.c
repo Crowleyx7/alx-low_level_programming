dde7915e6a9d3fa48b8d790e3f1de54d64b1471e
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-18 17:48:09 +0000

    k

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,18 +10,18 @@ int main(void)
 
 	for (num = 0; num < 9 ; num++)
 	{
-		for (num1 = num + 1; num1 < 10; num1++)
+	for (num1 = num + 1; num1 < 10; num1++)
+		{
+			num %= 10;
+			putchar(num + '0');
+			num1 %= 10;
+			putchar(num1 + '0');
+			if (num != 8 || num1 != 9)
 			{
-				num %= 10;
-				putchar(num + '0');
-				num1 %= 10;
-				putchar(num1 + '0');
-				if (num != 8 || num1 != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
