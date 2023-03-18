9ba7d47d772c1ee797ad76493836ea7dff7219b6
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-18 20:13:14 +0000

    k

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - printing all the possible combinations op two two digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num, num1, num2, num3;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (num1 = 0; num1 < 10; num1++)
+		{
+			num %= 10;
+			putchar(num + '0');
+			num1 %= 10;
+			putchar(num1 + '0');
+			putchar(' ');
+			for (num2 = 0; num2 < 10; num2++)
+			{
+				for (num3 = 0; num3 < 10; num3++)
+				{
+					num2 %= 10;
+					putchar(num2 + '0');
+					num3 %= 10;
+					putchar(num3 + '0');
+					if (num2 != 9 || num3 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}	
+			}	
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
+
+
