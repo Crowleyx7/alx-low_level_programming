26197f6fca3cb08d5bfa82f42cdff6812077bec7
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-18 20:55:56 +0000

    k

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,22 +10,24 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 	{
-		for (num1 = 0; num1 < 10; num1++)
+		for (num1 = 0 ; num1 < 10; num1++)
 		{
-			for (num2 = 0; num2 < 10; num2++)
+			for (num2 = num ; num2 < 10; num2++)
 			{
-				for (num3 = num2 + 1 ; num3 < 10; num3++)
+				for (num3 = num1 + 1 ; num3 < 10; num3++)
 				{
 					num %= 10;
 					putchar(num + '0');
 					num1 %= 10;
 					putchar(num1 + '0');
 					putchar(' ');
+
 					num2 %= 10;
 					putchar(num2 + '0');
 					num3 %= 10;
 					putchar(num3 + '0');
-					if (num2 != 9 || num3 != 9)
+
+					if (num != 9 || num1 != 9 || num2 != 9 || num3 != 9)
 					{
 						putchar(',');
 						putchar(' ');
