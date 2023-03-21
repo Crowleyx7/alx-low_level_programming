54e05dc9784e30270d86ccd52eba4b17dfefeabd
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-21 17:52:14 +0000

    k

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,12 @@ void print_alphabet_x10(void)
 	char ch;
 	int a;
 
-	for(a = 0; a < 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for(ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
-	}
 		_putchar('\n');
-	
+	}
 }
