09c8f12df3d35abc74ae29246535529e417195fb
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-21 17:10:14 +0000

    k

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,10 @@
  *
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
-	void print_alphabet(void)
-	{
-		char ch;
-		for (ch = 'a'; ch => 'z' ; ch++)
-		
-			_putchar(ch);
-		
-		_putchar('\n');
-	}
-	return (0);
+	char ch;
+	for (ch = 'a'; ch <= 'z' ; ch++)	
+		_putchar(ch);
+	_putchar('\n');
 }
