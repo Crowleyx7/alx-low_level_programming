5fa092e3a778612752212574a0084dc7f40ca335
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-21 17:25:21 +0000

    k

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int a;
+
+	for(a = 0; a < 10; a++)
+	{
+		for(ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
