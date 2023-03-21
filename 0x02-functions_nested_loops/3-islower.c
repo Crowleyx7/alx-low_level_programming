b3e508e790d386bd65ff6471b5c0d37288115029
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-21 18:02:16 +0000

    k

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - check for lower characters
+ */
+int _islower(int c)
+{
+	if ( c = 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if ( c = 'A' && c <= 'z')
+	{
+		return (0);
+	}
+}
