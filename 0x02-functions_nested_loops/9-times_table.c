8209d544053b48b0cbb9495729fcbb746c63033e
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-22 19:38:13 +0000

    k

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints time table
+ * @n: gets checked
+ * Return: 0
+ */
+void times_table(void)
+{
+	int a, b, r;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			r = a * b;
+			if ( r < 10)
+			{
+				_putchar( r + '0');
+				if ( b != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else if ( r >= 10)
+			{
+				_putchar(( r / 10) + '0');
+				_putchar(( r % 10) + '0');
+				if ( r != a * 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
