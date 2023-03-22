f125d695aee3f28619a3131c39a0a74868e8ceee
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-22 00:12:45 +0000

    k

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,16 +7,10 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar('+');
-		_putchar(',');
 		return (1);
 	else if (n < 0)
-		_putchar('-');
-		_putchar(',');
 		return (-1);
 	else
-		_putchar('0');
-		_putchar(',');
 		return (0);
 	return (0);
 }
