6d2b547d00bcb2dbb9a25c4aca45cef0ed950b8f
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-22 21:16:15 +0000

    k

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 			if ( r < 10)
 			{
 				_putchar( r + '0');
-				if ( b != 9)
+				if ( b != 9 )
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -27,7 +27,7 @@ void times_table(void)
 			{
 				_putchar(( r / 10) + '0');
 				_putchar(( r % 10) + '0');
-				if ( b != 9  )
+				if ( b != 9 && b != 0 )
 				{
 					_putchar(',');
 					_putchar(' ');
