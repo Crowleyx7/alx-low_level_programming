3c6fdff5ef8ff08e0e76c52207d70d58fa18a6a1
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-22 00:59:30 +0000

    k

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints lust digit of a number
+ * @n: gets checked
+ * Return: Always n
+ */
+int print_last_digit(int n)
+{
+	n %= 10;
+	reurn (n);
+}
