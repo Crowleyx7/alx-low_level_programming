42f68c6a0f091cfae3f81f3e4d775c2010d51478
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-23 23:51:51 +0000

    k

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 to 14
+ *
+ * Return: always 
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 4; a++)
+		{
+			_putchar(a + '0');
+			_putchar(b + '0');
+		}
+	}
+	_putchar('\n');
+}
