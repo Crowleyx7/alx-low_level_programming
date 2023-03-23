5b2e738cbdbe3f49599e044300fb293ca00f9f1f
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-23 23:37:23 +0000

    k

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	int a;
-	
-	for (a = 0; a < 10; a++)
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
 	{
-		_putchar(a + '0');	
+		_putchar(a);
 	}
 	_putchar('\n');
 }
