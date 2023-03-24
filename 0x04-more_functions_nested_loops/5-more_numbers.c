35f7461c5efd8d2807b493831505988196ebddc6
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-24 00:00:20 +0000

    k

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,17 @@ void more_numbers(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 4; b++)
+		_putchar(a + '0');
+		if ( a == 9)
 		{
-			_putchar(a + '0');
-			_putchar(b + '0');
+			for ( a = 1 ; a <= 1; a++)
+			{
+				for ( b = 0; b <= 4; a++);
+				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+				}
+			}
 		}
 	}
 	_putchar('\n');
