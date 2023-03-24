08e2cc9e278c8e62dead8b7c46d7350a4682ae9d
Crowleyx7 <alem.youssef7@gmail.com>
2023-03-24 03:01:13 +0000

    k

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,10 +11,22 @@ void print_diagonal(int n);
 	}
 	else
 	{
-		int i;
-		for (i = 1; i <= n; i++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			_putchar('\\');
+			for (j = 0; j < n; j++);
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
+			}
 		}
 	_putchar('\n');
 	}
+}
