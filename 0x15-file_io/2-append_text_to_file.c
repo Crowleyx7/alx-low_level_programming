fb094b98af96cfeb5531973550f38a6839db9054
crowley <alem.youssef7@gmail.com>
2023-08-07 22:23:11 +0100

    k

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,6 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
-
 	if (filename == NULL)
 		return (-1);
 
@@ -25,14 +24,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 
-
 	if (o == -1 || w == -1)
 		return (-1);
-
-
 	close(o);
-
-
 	return (1);
 }
-
